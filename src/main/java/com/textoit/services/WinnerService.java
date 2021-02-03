42a721cc618c69937a5be4676e9bcbf67b69882d
Welington Lisboa <welington.lisboa@prodeb.ba.gov.br>
2021-02-03 02:50:50 -0300

    getListMinMax

--- src/main/java/com/textoit/services/WinnerService.java
+++ src/main/java/com/textoit/services/WinnerService.java
@@ -1,7 +1,9 @@
 package com.textoit.services;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,27 +19,41 @@ public class WinnerService {
 
 	@Autowired
 	private WinnerRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public MinMaxIntervalProducerDTO getMinMax() {
-		
-		//winners with more than 1 win
+
+		// winners with more than 1 win
 		List<Winner> winners = repository.getWinners();
+
+		// All interval for each winner
+		List<IntervalProducerDTO> intervalForWinner = getIntervalForWinner(winners);
 		
+		intervalForWinner.sort(Comparator.comparing(IntervalProducerDTO::getInterval));
+		int intervalMin = intervalForWinner.get(0).getInterval();
+		int intervalMax = intervalForWinner.get(intervalForWinner.size() - 1).getInterval();
+
 		MinMaxIntervalProducerDTO result = new MinMaxIntervalProducerDTO();
-		
-		List<IntervalProducerDTO> min = new ArrayList<>();
-		min.add(new IntervalProducerDTO("Producer 1",1,2008,2009));
-		min.add(new IntervalProducerDTO("Producer 2",1,2018,2019));
-		
-		List<IntervalProducerDTO> max = new ArrayList<>();
-		max.add(new IntervalProducerDTO("Producer 1",99,1900,1999));
-		max.add(new IntervalProducerDTO("Producer 2",99,2000,2099));
-		
-		result.setMin(min);
-		result.setMax(max);
-		
+		result.setMin(intervalForWinner.stream().filter(d -> d.getInterval().equals(intervalMin)).collect(Collectors.toList()));
+		result.setMax(intervalForWinner.stream().filter(d -> d.getInterval().equals(intervalMax)).collect(Collectors.toList()));
+
 		return result;
+	}
+
+
+	private List<IntervalProducerDTO> getIntervalForWinner(List<Winner> winners) {
+		List<IntervalProducerDTO> intervalForProducer = new ArrayList<>();
+		int interval;
+		IntervalProducerDTO intervalProducer;
+		for (int i = 0; i < winners.size() - 1; i++) {
+			if (winners.get(i).getNameProducer().equals(winners.get(i + 1).getNameProducer())) {
+				interval = winners.get(i + 1).getYear() - winners.get(i).getYear();
+				intervalProducer = new IntervalProducerDTO(winners.get(i).getNameProducer(), interval,
+						winners.get(i).getYear(), winners.get(i + 1).getYear());
+				intervalForProducer.add(intervalProducer);
+			}
+		}
+		return intervalForProducer;
 	}
 
 }
